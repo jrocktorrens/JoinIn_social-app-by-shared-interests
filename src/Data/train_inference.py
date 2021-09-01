d574ccb420254913300aefca34de0fb1136ccd06
Ilia Altmark <iliaaltmark@gmail.com>
2021-09-01 16:30:18 +0300

    1

--- src/Data/train_inference.py
+++ src/Data/train_inference.py
@@ -15,6 +15,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 K = 5
 KNN = NearestNeighbors(n_neighbors=K + 1)
 DATA = None
+TRANSFORMER = None
 
 app = Flask(__name__)
 
@@ -24,9 +25,14 @@ def find_k_neighbors():
     """Receiving input and returning closest neigbors"""
     input_dict = json.loads(request.get_json())
 
-    result = KNN.kneighbors(pd.DataFrame(input_dict), return_distance=False)
+    if TRANSFORMER:
+        result = KNN.kneighbors(TRANSFORMER.fit(pd.DataFrame(input_dict)),
+                                return_distance=False)
 
-    return jsonify(preds=result[0][1:].tolist())
+        return jsonify(preds=result[0][1:].tolist())
+
+    else:
+        return 'Error! Please train the transformer first!'
 
 
 @app.route('/fit_k_neighbors', methods=['POST'])
@@ -37,13 +43,20 @@ def fit_k_neighbors():
 
     fit_(DATA)
 
+    return '200'
 
-def fit_(df=None):
-    numerical = df.describe().columns
-    categorical = set(df.columns) - set(numerical)
 
-    df_u = df[numerical + categorical].copy()
+def fit_transform_(df):
+    numerical = df.describe().columns.tolist()
+    categorical = list(set(df.columns) - set(numerical))
 
+    # numerical = ['age']
+    # categorical = ['sex', 'diet', 'smokes', 'location', 'pets', 'speaks']
+
+    # df_u = df[numerical.tolist() + list(categorical)].copy()
+    # df_u = df[numerical + categorical].copy()
+
+    df_u = df.copy()
     # pipeline for nominal features
     nominal_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='most_frequent')),
@@ -61,8 +74,16 @@ def fit_(df=None):
     ])
 
     # Transforming
-    X = transformers.transform(df_u)
+    X = transformers.fit_transform(df_u)
 
+    global TRANSFORMER
+    TRANSFORMER = transformers
+
+    return X
+
+
+def fit_(df):
+    X = fit_transform_(df)
     # fitting
     global KNN
     KNN = KNN.fit(X)
