7f4531ad6cbb10f63235df2c7b0affdc8cdbc191
omer ram <0meram95@gmail.com>
2021-09-01 12:44:03 +0300

    event list and event modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Nav from "./Components/layout/Nav"
-
+import Search from  "./Components/search/Search"
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Search/>
     </div>
   );
 }
